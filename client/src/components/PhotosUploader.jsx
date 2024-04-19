25f93ef419bef9a6faff7f420c517636cdb1e91e
1Ashutosh1 <at8969927@gmail.com>
2024-04-19 23:55:29 +0530

    Delete Photo Funcionality

--- client/src/components/PhotosUploader.jsx
+++ client/src/components/PhotosUploader.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
-import { IoCloudUploadOutline } from "react-icons/io5";
+import { IoCloudUploadOutline,IoTrash  } from "react-icons/io5";
+import { FaRegStar,FaStar  } from "react-icons/fa";
 import axios from 'axios';
 
 const PhotosUploader = ({addedPhotos,setAddedPhotos}) => {
@@ -34,6 +35,19 @@ const PhotosUploader = ({addedPhotos,setAddedPhotos}) => {
       });
   }
 
+  function removePhoto(ev,filename){
+    ev.preventDefault();
+    setAddedPhotos([...addedPhotos.filter(photo => photo !== filename)]);
+  }
+
+  function selectAsMainPhoto(ev,filename){
+    ev.preventDefault();
+    
+    const newAddedPhotos = ([filename,...addedPhotos
+      .filter(photo => photo !== filename)]);
+    setAddedPhotos(newAddedPhotos);
+  }
+
   return (
     <>
       <div className="flex gap-2">
@@ -53,12 +67,20 @@ const PhotosUploader = ({addedPhotos,setAddedPhotos}) => {
       <div className="mt-2 grid gap-2 grid-cols-3 md:grid-cols-4 lg:grid-cols-6">
         {addedPhotos.length > 0 &&
           addedPhotos.map((link) => (
-            <div className="h-32 flex" key={link}>
+            <div className="h-32 flex relative" key={link}>
               <img
                 className="rounded-2xl w-full object-cover position"
                 src={"http://localhost:4000/uploads/" + link}
                 alt=""
               />
+              <button onClick={(ev) => removePhoto(ev,link)} className="cursor-pointer absolute bottom-1 right-1 text-white bg-black bg-opacity-50 rounded-2xl py-2 px-3">
+              <IoTrash  />
+              </button>
+
+              <button onClick={(ev) => selectAsMainPhoto(ev,link)} className="cursor-pointer absolute bottom-1 left-1 text-white bg-black bg-opacity-50 rounded-2xl py-2 px-3">
+                {link === addedPhotos[0] && <FaStar />}
+                {link !== addedPhotos[0] && <FaRegStar />}
+              </button>
             </div>
           ))}
         <label className="h-32 flex items-center gap-1 justify-center border bg-transparent rounded-2xl text-2xl p-2 text-gray-600 cursor-pointer">
